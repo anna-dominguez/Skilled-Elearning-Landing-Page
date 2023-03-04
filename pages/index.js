95d52faf9c834e2ae819e4da4fadb8da06c3caef
Vesta-nna <anna.dominguez.anna@gmail.com>
2023-03-04 01:24:06 +0100

    adjust margin

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Image from 'next/image'
 
-import useMatchMedia from 'react-use-match-media'
-
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import Main from '@/components/Main'
